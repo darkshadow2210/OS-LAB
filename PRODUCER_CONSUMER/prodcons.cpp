67ce2a08b59444b45c3e0950226e540d38d0d0a9
Guttula Vijay <93029132+VJLIVE@users.noreply.github.com>
2025-06-09 22:40:05 +0530

    Update prodcons.cpp

--- PRODUCER_CONSUMER/prodcons.cpp
+++ PRODUCER_CONSUMER/prodcons.cpp
@@ -33,12 +33,14 @@ int main(){
 	}
 	return 0;
 }
+
 int wait(int s){
 	return(--s);
 }
-	int signal(int s){
-		return(++s);
-	}
+int signal(int s){
+	return(++s);
+}
+
 void producer(){
 	mutex = wait(mutex);
 	full=signal(full);
